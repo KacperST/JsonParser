258ebff914c2e74583362a78950186540c58b927
KacperSt <kstankiewicz@student.agh.edu.pl>
2024-04-16 19:29:02 +0200

    Add basic case

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
+func read_json(filename string) ([]byte, error) {
+	// Read the json file
+	fileBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return fileBytes, nil
+}
+
+func parse_json(fileBytes []byte) (output bool, err error) {
+	var awsIAMRolePolicy AWSIAMRolePolicy
+	err = json.Unmarshal(fileBytes, &awsIAMRolePolicy)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	fileBytes, err := read_json("/home/kacperek/ZadaniaNaStaz/Remitly/JsonParser/inputs/aws_iam_role_policy.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	output, err := parse_json(fileBytes)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(output)
 }
